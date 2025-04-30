20a4a36084b956453929adb0e0f86641d0cf8e69
mahdiisabry1 <mahdiisabry@gmail.com>
2025-04-30 13:15:56 +0530

    Update RegistrationBody.java

--- techstore-backend/src/main/java/com/store/techstore_backend/api/model/RegistrationBody.java
+++ techstore-backend/src/main/java/com/store/techstore_backend/api/model/RegistrationBody.java
@@ -1,5 +1,10 @@
 package com.store.techstore_backend.api.model;
 
 public class RegistrationBody {
+    private String username;
+    private String email;
+    private String password;
+    private String fastName;
+    private String lastName;
 
 }
